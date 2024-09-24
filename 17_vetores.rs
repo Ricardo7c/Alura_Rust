0d6b06900dd8a45813313184581fa748b149ce03
Ricardo Silva <ricardo7c@hotmail.com>
2024-09-24 14:42:14 -0300

    Aulas de vetores finalizada

--- 17_vetores.rs
+++ 17_vetores.rs
@@ -1,4 +1,5 @@
 fn main(){
+    //let mut notas: Vec<f32> = Vec::with_capacity(4);
     let mut notas: Vec<f32> = Vec::new();
     notas.push(10.0);
     notas.push(8.8);
@@ -10,9 +11,11 @@ fn main(){
 
     println!("-------------------------------");
     let mut notas2 : Vec<f32> = vec![2.3, 5.4, 6.4];
+    println!("Capacidade = {}", notas2.capacity());
     println!("{:?}", notas2);
 
     notas2.push(7.3);
+    println!("Capacidade = {}", notas2.capacity());
     println!("Nota 1 = {}", notas2[0]);
 
     
@@ -21,6 +24,16 @@ fn main(){
         None => 0.0
     });
 
+    println!("-------------POP--------------");
+    while let Some(nota) = notas2.pop(){
+        println!("Valor removido = {}", nota);
+        println!("{:?}", notas2)
+    }
+
+    for nota in &notas{
+        println!("Nota = {}", nota);
+    }
+    println!("{:?}", notas)
 
     
 }
